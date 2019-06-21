a22779cc0e2cb61baf702ba5861a38eef451aa6e
THANHDNV <vanthanh971125@gmail.com>
2019-06-21 20:32:41 +0700

    designing the logo for the website + first step at building the navbar

--- include/header.php
+++ include/header.php
@@ -12,8 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel='stylesheet' type='text/stylesheet' href='assets/css/bootstrap.min.css'>
-    <link rel='stylesheet' type='text/stylesheet' href='assets/css/style.css'>
+    <link rel='stylesheet' type='text/css' href='assets/css/bootstrap.min.css'>
+    <link rel='stylesheet' type='text/css' href='assets/css/style.css'>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src='assets/js/bootstrap.bundle.min.js'></script>
@@ -25,4 +25,8 @@
         <div class='logo'>
             <a href='index.php'>Swiftfeed</a>
         </div>
+
+        <nav>
+
+        </nav>
     </div>
